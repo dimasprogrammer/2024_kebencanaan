7f7041c673c2a380bb7ab9cf90299fb278f842aa
dimasprogrammer <dimasworking789@gmail.com>
2024-07-22 09:29:58 +0700

    Memperbaiki Inputan User

---
+++ application/logs/log-2024-07-22.php
@@ -0,0 +1,42 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-07-22 09:07:29 --> Severity: Warning --> pg_query(): Query failed: ERROR:  invalid input syntax for type bigint: &quot;&quot;
+LINE 1: ..._ip&quot;) VALUES ('482FF5841F0A018CB8E62B65F19F2154', '', '', 'u...
+                                                             ^ D:\APPLICATION\PHP\PHP8\2024\2024_kebencanaan\system\database\drivers\postgre\postgre_driver.php 234
+ERROR - 2024-07-22 09:07:29 --> Query error: ERROR:  invalid input syntax for type bigint: ""
+LINE 1: ..._ip") VALUES ('482FF5841F0A018CB8E62B65F19F2154', '', '', 'u...
+                                                             ^ - Invalid query: INSERT INTO "xi_sa_users" ("token", "id_instansi", "id_regency", "username", "password", "fullname", "foto_profile", "blokir", "id_status", "validate_email_code", "validate_email_status", "reset_password_code", "reset_password_status", "reset_password_expired", "create_by", "create_date", "create_ip", "mod_by", "mod_date", "mod_ip") VALUES ('482FF5841F0A018CB8E62B65F19F2154', '', '', 'ucupsalah', '$2a$12$hqpiAR9NXv0i7pRwg7or.O0tmoilUQfIi1W47qJpg7qboOpDNr13O', 'Ucup Salah', 'default-user-icon.jpg', '0', '1', '', 0, '', 0, 0, 'michael', '2024-07-22 09:07:28', '::1', 'michael', '2024-07-22 09:07:28', '::1')
+ERROR - 2024-07-22 09:07:43 --> 404 Page Not Found: /index
+ERROR - 2024-07-22 09:07:43 --> 404 Page Not Found: /index
+ERROR - 2024-07-22 09:07:46 --> 404 Page Not Found: /index
+ERROR - 2024-07-22 09:07:46 --> 404 Page Not Found: /index
+ERROR - 2024-07-22 09:08:09 --> 404 Page Not Found: /index
+ERROR - 2024-07-22 09:08:09 --> 404 Page Not Found: /index
+ERROR - 2024-07-22 09:08:46 --> Severity: Warning --> pg_query(): Query failed: ERROR:  invalid input syntax for type bigint: &quot;&quot;
+LINE 1: ..._ip&quot;) VALUES ('F235E2BF036A7CB9F2F53688DC3EA85C', '', '', 'u...
+                                                             ^ D:\APPLICATION\PHP\PHP8\2024\2024_kebencanaan\system\database\drivers\postgre\postgre_driver.php 234
+ERROR - 2024-07-22 09:08:46 --> Query error: ERROR:  invalid input syntax for type bigint: ""
+LINE 1: ..._ip") VALUES ('F235E2BF036A7CB9F2F53688DC3EA85C', '', '', 'u...
+                                                             ^ - Invalid query: INSERT INTO "xi_sa_users" ("token", "id_instansi", "id_regency", "username", "password", "fullname", "foto_profile", "blokir", "id_status", "validate_email_code", "validate_email_status", "reset_password_code", "reset_password_status", "reset_password_expired", "create_by", "create_date", "create_ip", "mod_by", "mod_date", "mod_ip") VALUES ('F235E2BF036A7CB9F2F53688DC3EA85C', '', '', 'ucupsalah', '$2a$12$o4h7HlocPwFs03lVqz1HPuoHEhAkWTYhadX0H4ufXT1U2gC7HVmdK', 'Ucup Salah', 'default-user-icon.jpg', '0', '1', '', 0, '', 0, 0, 'michael', '2024-07-22 09:08:46', '::1', 'michael', '2024-07-22 09:08:46', '::1')
+ERROR - 2024-07-22 09:12:46 --> 404 Page Not Found: /index
+ERROR - 2024-07-22 09:12:46 --> 404 Page Not Found: /index
+ERROR - 2024-07-22 09:13:09 --> Severity: Warning --> pg_query(): Query failed: ERROR:  invalid input syntax for type bigint: &quot;&quot;
+LINE 1: ..._ip&quot;) VALUES ('6BF4DABC7C2E0FF08AC1175C1FC053DC', '', 'ucups...
+                                                             ^ D:\APPLICATION\PHP\PHP8\2024\2024_kebencanaan\system\database\drivers\postgre\postgre_driver.php 234
+ERROR - 2024-07-22 09:13:09 --> Query error: ERROR:  invalid input syntax for type bigint: ""
+LINE 1: ..._ip") VALUES ('6BF4DABC7C2E0FF08AC1175C1FC053DC', '', 'ucups...
+                                                             ^ - Invalid query: INSERT INTO "xi_sa_users" ("token", "id_instansi", "username", "password", "fullname", "foto_profile", "blokir", "id_status", "validate_email_code", "validate_email_status", "reset_password_code", "reset_password_status", "reset_password_expired", "create_by", "create_date", "create_ip", "mod_by", "mod_date", "mod_ip") VALUES ('6BF4DABC7C2E0FF08AC1175C1FC053DC', '', 'ucupsalah', '$2a$12$KsAODYp1dkR8ZrV5E8cwu.5yRsZ/0MDdgum10vm9YeL3YIAtjHtuO', 'Ucup Salah', 'default-user-icon.jpg', '0', '1', '', 0, '', 0, 0, 'michael', '2024-07-22 09:13:09', '::1', 'michael', '2024-07-22 09:13:09', '::1')
+ERROR - 2024-07-22 09:13:15 --> 404 Page Not Found: /index
+ERROR - 2024-07-22 09:13:15 --> 404 Page Not Found: /index
+ERROR - 2024-07-22 09:13:17 --> 404 Page Not Found: /index
+ERROR - 2024-07-22 09:13:17 --> 404 Page Not Found: /index
+ERROR - 2024-07-22 09:14:40 --> 404 Page Not Found: /index
+ERROR - 2024-07-22 09:14:40 --> 404 Page Not Found: /index
+ERROR - 2024-07-22 09:15:01 --> Severity: Warning --> pg_query(): Query failed: ERROR:  invalid input syntax for type bigint: &quot;&quot;
+LINE 1: ..._ip&quot;) VALUES ('5EF825AF5090BAF88B2EADF212045DA8', '', '', 'u...
+                                                             ^ D:\APPLICATION\PHP\PHP8\2024\2024_kebencanaan\system\database\drivers\postgre\postgre_driver.php 234
+ERROR - 2024-07-22 09:15:01 --> Query error: ERROR:  invalid input syntax for type bigint: ""
+LINE 1: ..._ip") VALUES ('5EF825AF5090BAF88B2EADF212045DA8', '', '', 'u...
+                                                             ^ - Invalid query: INSERT INTO "xi_sa_users" ("token", "id_instansi", "id_regency", "username", "password", "fullname", "foto_profile", "blokir", "id_status", "validate_email_code", "validate_email_status", "reset_password_code", "reset_password_status", "reset_password_expired", "create_by", "create_date", "create_ip", "mod_by", "mod_date", "mod_ip") VALUES ('5EF825AF5090BAF88B2EADF212045DA8', '', '', 'ucupsalah', '$2a$12$YeMYEvhktErh3Tt9dv07YuGwScQAah/Lw6lExu9ekCwhlNYTB2RuK', 'Ucup Salah', 'default-user-icon.jpg', '0', '1', '', 0, '', 0, 0, 'michael', '2024-07-22 09:15:00', '::1', 'michael', '2024-07-22 09:15:00', '::1')
+ERROR - 2024-07-22 09:17:01 --> 404 Page Not Found: /index
+ERROR - 2024-07-22 09:17:01 --> 404 Page Not Found: /index
